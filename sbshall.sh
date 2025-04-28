cbfff4f6d86ba1c4a62bd6450d945cd3c3dbf3bb
zsm <50068931+zming66@users.noreply.github.com>
2025-04-28 09:53:22 +0800

    Update sbshall.sh

--- sbshall.sh
+++ sbshall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 定义主脚本的下载URL
 DEBIAN_MAIN_SCRIPT_URL="https://gh-proxy.com/https://raw.githubusercontent.com/qljsyph/sbshell/refs/heads/main/debian/menu.sh"
-OPENWRT_MAIN_SCRIPT_URL="https://gh-proxy.com/https://raw.githubusercontent.com/qljsyph/sbshell/refs/heads/main/openwrt/menu.sh"
+OPENWRT_MAIN_SCRIPT_URL="https://gh-proxy.com/https://raw.githubusercontent.com/zming66/zsm/refs/heads/main/openwrt/menu.sh"
  
 # 脚本下载目录
 SCRIPT_DIR="/etc/sing-box/scripts"
