1f3e4baf3db2f7bb6202f3b9b8a1c3004cd860fd
zsm <50068931+zming66@users.noreply.github.com>
2025-06-09 15:42:56 +0800

    Create fetch_cnipv6.py

---
+++ scripts/fetch_cnipv6.py
@@ -0,0 +1,22 @@
+ requests
+import json
+
+def fetch_ipv6_list():
+    # 数据源1：APNIC最新分配记录
+    apnic_url = "http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest"
+    # 数据源2：备用CHN路由项目
+    backup_url = "https://raw.githubusercontent.com/allenzha/chn-iplist/master/chnroute-ipv6.txt"
+    
+    try:
+        r = requests.get(apnic_url)
+        china_ips = [line for line in r.text.splitlines() 
+                    if 'CN|ipv6' in line]
+        with open('data/raw_ipv6.txt', 'w') as f:
+            f.write('\n'.join(china_ips))
+    except:
+        r = requests.get(backup_url)
+        with open('data/raw_ipv6.txt', 'w') as f:
+            f.write(r.text)
+
+if __name__ == '__main__':
+    fetch_ipv6_list()
